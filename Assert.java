d595f9dd17b5766b27d15be1b711ab5d57fce9b9
SwathiPriya2000 <63891711+SwathiPriya2000@users.noreply.github.com>
2020-09-12 23:06:52 +0530

    Add files via upload

---
+++ Assert.java
@@ -0,0 +1,8 @@
+class Assert {
+  public static void main(String args[]) {
+    String[] weekends = {"Friday", "Saturday", "Sunday"};
+    assert weekends.length==2 : "There are only 2 weekends in a week";
+    System.out.println("There are " + weekends.length + "  weekends in a week");
+  }
+}
+
